4311b08ce2abf2a8a5d24242fb7623a511f3f5f1
AlexanderGildenstern <a.gildenstern@gmx.de>
2021-10-11 10:56:40 +0200

    added slideShow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'alexandergildenstern';
+export class AppComponent implements OnInit {
+  images = ['background.jpg', 'code.jpg', 'mac.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateimage();
+  }
+
+  updateimage(){
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setInterval(() =>{
+        this.showImage = true;
+      },10);
+
+    }, 8000)
+
+  }
 }
